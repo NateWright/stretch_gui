846941b34af2e7532658f18947eeee87dcbf9b60
Nate Wright <nathanwrightbusiness@gmail.com>
2022-08-04 12:46:33 -0400

    test region growth

--- stretch_gui_library/src/ObjectSegmenter.cpp
+++ stretch_gui_library/src/ObjectSegmenter.cpp
@@ -18,18 +18,24 @@ void ObjectSegmenter::segmentAndFind(const pcl::PointCloud<point>::Ptr& inputClo
     voxelFilter.filter(*vox_filtered_cloud);
 
     pcl::search::Search<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB>);
-    pcl::IndicesPtr indices(new std::vector<int>);
+    pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
+    pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> normal_estimator;
+    normal_estimator.setSearchMethod(tree);
+    normal_estimator.setInputCloud(vox_filtered_cloud);
+    normal_estimator.setKSearch(50);
+    normal_estimator.compute(*normals);
 
-    pcl::RegionGrowingRGB<pcl::PointXYZRGB> reg;
-    reg.setInputCloud(vox_filtered_cloud);
+    pcl::RegionGrowing<pcl::PointXYZRGB, pcl::Normal> reg;
+    reg.setMinClusterSize(50);
+    reg.setMaxClusterSize(1000000);
     reg.setSearchMethod(tree);
-    reg.setDistanceThreshold(10);
-    reg.setPointColorThreshold(6);
-    reg.setRegionColorThreshold(5);
-    reg.setMinClusterSize(10);
+    reg.setNumberOfNeighbours(30);
+    reg.setInputCloud(vox_filtered_cloud);
+    reg.setInputNormals(normals);
+    reg.setSmoothnessThreshold(3.0 / 180.0 * M_PI);
+    reg.setCurvatureThreshold(1.0);
 
     reg.extract(clusters);
-
     segmented_cloud = reg.getColoredCloud();
     segmented_cloud->header = inputCloud->header;
     testPub_.publish(segmented_cloud);
@@ -89,108 +95,34 @@ void ObjectSegmenter::segmentAndFind(const pcl::PointCloud<point>::Ptr& inputClo
     clusterPub_.publish(cloud_cluster);
 }
 
-// void ObjectSegmenter::segmentation(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& pc) {
-//     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud = pc;
-
-//     pcl::search::Search<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB>);
-//     pcl::IndicesPtr indices(new std::vector<int>);
-//     pcl::removeNaNFromPointCloud(*cloud, *indices);
-
-//     pcl::RegionGrowingRGB<pcl::PointXYZRGB> reg;
-//     reg.setInputCloud(cloud);
-//     reg.setIndices(indices);
-//     reg.setSearchMethod(tree);
-//     reg.setDistanceThreshold(10);
-//     reg.setPointColorThreshold(6);
-//     reg.setRegionColorThreshold(5);
-//     reg.setMinClusterSize(600);
-
-//     reg.extract(clusters_);
-
-//     colored_cloud_ = reg.getColoredCloud();
-
-//     colored_cloud_->header.frame_id = pc->header.frame_id;
-
-//     return;
-// }
-
-// void ObjectSegmenter::segmentation(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& pc) {
-//     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud = pc;
-
-//     pcl::search::Search<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB>);
-//     pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
-//     pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> normal_estimator;
-//     normal_estimator.setSearchMethod(tree);
-//     normal_estimator.setInputCloud(cloud);
-//     normal_estimator.setKSearch(50);
-//     normal_estimator.compute(*normals);
-
-//     pcl::IndicesPtr indices(new std::vector<int>);
-//     pcl::removeNaNFromPointCloud(*cloud, *indices);
-
-//     pcl::RegionGrowing<pcl::PointXYZRGB, pcl::Normal> reg;
-//     reg.setMinClusterSize(50);
-//     reg.setMaxClusterSize(1000000);
-//     reg.setSearchMethod(tree);
-//     reg.setNumberOfNeighbours(30);
-//     reg.setInputCloud(cloud);
-//     reg.setIndices(indices);
-//     reg.setInputNormals(normals);
-//     reg.setSmoothnessThreshold(3.0 / 180.0 * M_PI);
-//     reg.setCurvatureThreshold(1.0);
-
-//     reg.extract(clusters_);
-
-//     colored_cloud_ = reg.getColoredCloud();
-
-//     colored_cloud_->header.frame_id = pc->header.frame_id;
-
-//     return;
-// }
-
-// void ObjectSegmenter::segmentation(const pcl::PointCloud<point>::Ptr& inputCloud) {
+// void ObjectSegmenter::segmentAndFind(const pcl::PointCloud<point>::Ptr& inputCloud, const point pointToFind) {
 //     pcl::PointCloud<point>::Ptr vox_filtered_cloud(new pcl::PointCloud<point>);
-//     pcl::PointCloud<point>::Ptr table_filtered_cloud(new pcl::PointCloud<point>);
+//     pcl::PointCloud<point>::Ptr segmented_cloud(new pcl::PointCloud<point>);
+//     std::vector<pcl::PointIndices> clusters;
 
 //     // Down sample the point cloud
 //     pcl::VoxelGrid<point> voxelFilter;
 //     voxelFilter.setInputCloud(inputCloud);
-//     voxelFilter.setLeafSize(0.01f, 0.01f, 0.01f);
+//     voxelFilter.setLeafSize(0.015f, 0.015f, 0.015f);
 //     voxelFilter.filter(*vox_filtered_cloud);
 
-//     // Remove table from scene
-//     pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
-//     pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
-//     pcl::SACSegmentation<point> seg1;
+//     pcl::search::Search<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB>);
+//     pcl::IndicesPtr indices(new std::vector<int>);
 
-//     seg1.setOptimizeCoefficients(true);
-//     seg1.setModelType(pcl::SACMODEL_PLANE);
-//     seg1.setMethodType(pcl::SAC_RANSAC);
-//     seg1.setDistanceThreshold(0.01);
+//     pcl::RegionGrowingRGB<pcl::PointXYZRGB> reg;
+//     reg.setInputCloud(vox_filtered_cloud);
+//     reg.setSearchMethod(tree);
+//     reg.setDistanceThreshold(10);
+//     reg.setPointColorThreshold(6);
+//     reg.setRegionColorThreshold(5);
+//     reg.setMinClusterSize(10);
 
-//     seg1.setInputCloud(vox_filtered_cloud);
-//     seg1.segment(*inliers, *coefficients);
+//     reg.extract(clusters);
 
-//     pcl::ExtractIndices<point> extract;
-//     extract.setInputCloud(vox_filtered_cloud);
-//     extract.setIndices(inliers);
-//     extract.setNegative(true);
-//     extract.filter(*table_filtered_cloud);
+//     segmented_cloud = reg.getColoredCloud();
+//     segmented_cloud->header = inputCloud->header;
+//     testPub_.publish(segmented_cloud);
 
-//     pcl::search::KdTree<point>::Ptr tree(new pcl::search::KdTree<point>);
-//     tree->setInputCloud(table_filtered_cloud);
-
-//     std::vector<pcl::PointIndices> cluster_indices;
-//     pcl::EuclideanClusterExtraction<point> ec;
-//     ec.setClusterTolerance(0.02);  // 2cm
-//     ec.setMinClusterSize(100);
-//     ec.setMaxClusterSize(25000);
-//     ec.setSearchMethod(tree);
-//     ec.setInputCloud(table_filtered_cloud);
-//     ec.extract(cluster_indices);
-// }
-
-// void ObjectSegmenter::findCluster(int posX, int posY) {
 //     ROS_INFO_STREAM("Picking event occurred");
 
 //     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster;
@@ -198,12 +130,19 @@ void ObjectSegmenter::segmentAndFind(const pcl::PointCloud<point>::Ptr& inputClo
 //     ROS_INFO_STREAM("Looking for cluster");
 //     bool done = false;
 
-//     int pos = posY * colored_cloud_->width + posX;
+//     pcl::KdTreeFLANN<pcl::PointXYZRGB> kdtree;
+//     kdtree.setInputCloud(segmented_cloud);
+//     std::vector<int> pointIdxNKNSearch(1);
+//     std::vector<float> pointNKNSquaredDistance(1);
 
-//     for (pcl::PointIndices p : clusters_) {
+//     kdtree.nearestKSearch(pointToFind, 1, pointIdxNKNSearch, pointNKNSquaredDistance);
+
+//     int pos = pointIdxNKNSearch[0];
+
+//     for (pcl::PointIndices p : clusters) {
 //         cloud_cluster.reset(new pcl::PointCloud<pcl::PointXYZRGB>);
 //         for (const auto& idx : p.indices) {
-//             cloud_cluster->push_back((*colored_cloud_)[idx]);
+//             cloud_cluster->push_back((*segmented_cloud)[idx]);
 //             if (idx == pos) {
 //                 cloud_cluster->width = cloud_cluster->size();
 //                 cloud_cluster->height = 1;
@@ -232,9 +171,9 @@ void ObjectSegmenter::segmentAndFind(const pcl::PointCloud<point>::Ptr& inputClo
 //     pStamped.point.x = x / count;
 //     pStamped.point.y = y / count;
 //     pStamped.point.z = z / count;
-//     pStamped.header.frame_id = colored_cloud_->header.frame_id;
+//     pStamped.header.frame_id = inputCloud->header.frame_id;
 //     pointPub_.publish(pStamped);
 
-//     cloud_cluster->header.frame_id = colored_cloud_->header.frame_id;
+//     cloud_cluster->header.frame_id = inputCloud->header.frame_id;
 //     clusterPub_.publish(cloud_cluster);
 // }
