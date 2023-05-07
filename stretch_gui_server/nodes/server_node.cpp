b04988b9728c14f4fbf253ba7a006ba9446172d9
NateWright <nathanwrightbusiness@gmail.com>
2023-05-07 19:05:38 -0400

    added ip as a param

--- stretch_gui_server/nodes/server_node.cpp
+++ stretch_gui_server/nodes/server_node.cpp
@@ -18,21 +18,25 @@ void closeApplication(int signal) {
 
 int main(int argc, char *argv[]) {
     ros::init(argc, argv, "stretch_gui_server");
+    ros::NodeHandlePtr nh(new ros::NodeHandle("stretch_gui_server"));
     app = new QCoreApplication(argc, argv);
-    Server server;
+    Server server(nh);
 
     // https://www.codegrepper.com/code-examples/whatever/qt+get+local+ip+address
     QString localhostname = QHostInfo::localHostName();
     QString ip = "tcp://";
-    QList<QHostAddress> hostList = QNetworkInterface::allAddresses();
-    for (const QHostAddress &address : hostList) {
-        if (address.protocol() == QAbstractSocket::IPv4Protocol && address.isLoopback() == false) {
-            ip += address.toString();
-            break;
-        }
-    }
+    // QList<QHostAddress> hostList = QNetworkInterface::allAddresses();
+    // for (const QHostAddress &address : hostList) {
+    //     if (address.protocol() == QAbstractSocket::IPv4Protocol && address.isLoopback() == false) {
+    //         ip += address.toString();
+    //         break;
+    //     }
+    // }
+    std::string s;
+    nh->getParam("/stretch_gui/ip", s);
+    ip += s;
     qDebug() << "Localhost name: " << localhostname;
-    ip += ":stretch";
+    ip += ":9999";
     qDebug() << "IP = " << ip;
     QRemoteObjectHost srcNode(QUrl(ip), nullptr);
     srcNode.enableRemoting(&server);
